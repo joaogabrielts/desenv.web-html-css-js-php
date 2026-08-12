218da74ad562bb207266f33a2837128e61c086cf
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2026-08-12 18:17:41 -0300

    JavaScript na prática finalizado

---
+++ Tema 5 - Linguagem Javascript/JsNaPratica.js
@@ -0,0 +1,12 @@
+
+const divisao = (a,b) => {
+
+let   resultado = a / b
+   return resultado
+  
+}
+let numero1 = prompt("Digite um número positivo")
+let numero2 = prompt("Digite outro numero positivo ")
+
+  let resultado = divisao(numero1, numero2)
+alert(`Resultado na divisão é : ${resultado}`)
