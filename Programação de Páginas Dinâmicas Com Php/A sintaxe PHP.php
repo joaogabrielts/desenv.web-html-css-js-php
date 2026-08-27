0528319e11a024073db0a9da12d33686dfb1c1c1
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2026-08-27 18:40:37 -0300

    Aula finalizada Entrada de dados: variáveis de requisição HTTP

---
+++ Programação de Páginas Dinâmicas Com Php/A sintaxe PHP.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>A sintaxe PHP</title>
+</head>
+
+<body>
+
+
+  <h1><?php $nome = "Marcos Oliveira";
+  echo "Seu nome é $nome" ?></h1>
+    <h1><?php $idade = 21;
+  echo "Seu nome é $idade" ?></h1>
+</body>
+
+</html>
