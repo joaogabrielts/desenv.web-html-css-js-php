c559fa1e0106d03b65672c354fc361aaa78ede6a
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2026-08-27 17:02:37 -0300

    Aula finalizada A linguagem PHP

---
+++ Programação de Páginas Dinâmicas Com Php/Conceitos básicos de PHP.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>A linguagem PHP</title>
+  </head>
+  <body>
+    <?php echo "My first PHP Script"; ?>
+  </body>
+</html>
